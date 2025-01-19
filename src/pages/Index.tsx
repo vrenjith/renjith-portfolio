d7f38d930b0ede01df6a7cc65b08163ce5e0cbed
Renjith Pillai <renjith.pillai@sap.com>
2025-01-19 22:06:55 +0530

    Remove WorkHistorySection and replace with ExperienceSection in Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,26 +1,26 @@
 import { motion } from "framer-motion";
 import { HeroSection } from "@/components/HeroSection";
-import { WorkHistorySection } from "@/components/WorkHistorySection";
+import { ExperienceSection } from "@/components/ExperienceSection";
 import { ProjectsSection } from "@/components/ProjectsSection";
 import { BlogsSection } from "@/components/BlogsSection";
 import { ContactSection } from "@/components/ContactSection";
 import { GoToTopButton } from "@/components/GoToTopButton";
 
 const Index = () => {
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-    >
-      <HeroSection />
-      <WorkHistorySection />
-      <ProjectsSection />
-      <BlogsSection />
-      <ContactSection />
-      <GoToTopButton />
-    </motion.div>
-  );
+    return (
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+        >
+            <HeroSection />
+            <ExperienceSection />
+            <ProjectsSection />
+            <BlogsSection />
+            <ContactSection />
+            <GoToTopButton />
+        </motion.div>
+    );
 };
 
 export default Index;
